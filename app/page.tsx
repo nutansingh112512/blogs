12ab8461ff325ba54db4715e940f78ddcf61ae19
Nutan Singh <nutankindtech.co.in@Nutans-MacBook-Air.local>
2025-11-10 12:56:31 +0530

    added home navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/blogs");
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans w-full">
-      home page
-    </div>
+    <div className="flex min-h-screen items-center justify-center font-sans w-full"></div>
   );
 }
